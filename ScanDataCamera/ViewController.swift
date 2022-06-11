cc412b106e10b12191c507ff398bf773ad54fff9
David Razmadze <razd45@gmail.com>
2022-06-11 11:34:40 -0400

    2. Create button with action

--- ScanDataCamera/ViewController.swift
+++ ScanDataCamera/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
+  
+  
+  @IBAction func scanPressed(_ sender: Any) {
+    print("scanning")
+  }
+  
 
 }
 
