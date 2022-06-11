f9b1245d70cdeb76443a3e6537dc9b3daf75acee
David Razmadze <razd45@gmail.com>
2022-06-11 12:21:20 -0400

    5. didTapOn - open URL or copy text to clipboard

--- ScanDataCamera/ViewController.swift
+++ ScanDataCamera/ViewController.swift
@@ -9,40 +9,63 @@ import UIKit
 import VisionKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  /// Checks is `isSupported` and `isAvailable` for `DataScannerViewController`
+  var scannerAvailable: Bool {
+      DataScannerViewController.isSupported &&
+      DataScannerViewController.isAvailable
+  }
 
+  // MARK: - Life cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
   
+  // MARK: - IBActions
+  
   @IBAction func scanPressed(_ sender: Any) {
-    guard checkPermissions() == true else { return }
+    guard scannerAvailable == true else { return }
     
     let regonizedDataTypes:Set<DataScannerViewController.RecognizedDataType> = [
-      .text()
+      .text(),
+      .barcode()
     ]
     
-    let dataScanner = DataScannerViewController(recognizedDataTypes: regonizedDataTypes)
+    let dataScanner = DataScannerViewController(recognizedDataTypes: regonizedDataTypes, isHighlightingEnabled: true)
+    dataScanner.delegate = self
     present(dataScanner, animated: true) {
       try? dataScanner.startScanning()
     }
   }
   
-  /// Checks is `isSupported` and `isAvailable`
-  private func checkPermissions() -> Bool {
-    // 2018 or newer with Neural Engine
-    print("DataScannerViewController.isSupported: \(DataScannerViewController.isSupported)")
-    
-    // ✅ Make sure Camera is enabled
-    print("DataScannerViewControllerisAvailable: \(DataScannerViewController.isAvailable)")
-    
-    if DataScannerViewController.isSupported && DataScannerViewController.isAvailable {
-      return true
-    } else {
-      return false
+}
+
+// MARK: - DataScannerViewControllerDelegate
+
+/// Delegate methods for `DataScannerViewController`
+extension ViewController: DataScannerViewControllerDelegate {
+  
+  /// User taps on the screen. Copy the text that was highlighted
+  func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
+    switch item {
+    case .text(let text):
+      print("text: \(text.transcript)")
+      UIPasteboard.general.string = text.transcript
+      dataScanner.stopScanning()
+      dataScanner.dismiss(animated: true)
+    case .barcode(let code):
+      guard let urlString = code.payloadStringValue, let url = URL(string: urlString) else { return }
+      UIApplication.shared.open(url)
+      dataScanner.stopScanning()
+      dataScanner.dismiss(animated: true)
+    default:
+      print("Unexpected item")
     }
   }
   
-
 }
 
